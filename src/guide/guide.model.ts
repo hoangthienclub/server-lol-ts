24365e6d0f882458391d94db6c52fa510bf77a4d
Tran Thien <thien.tran@saigonvalley.com>
2021-08-26 23:13:45 +0700

    create guide

--- src/guide/guide.model.ts
+++ src/guide/guide.model.ts
@@ -2,48 +2,23 @@ import * as mongoose from 'mongoose';
 import Champion from './summoner.interface';
 
 const guideSchema = new mongoose.Schema({
+  version: {
+    type: String,
+    default: '11.16.1'
+  },
   championId: Number,
+  name: String,
+  path: String,
   items: Object,
-  skills: Object,
-  summners: Object,
+  summoners: Object,
   runes: Object,
   position: Number,
-  content: String,
+  skills: Object,
+  introduce: String,
+  play: Object,
+  videos: Object,
 });
 
 const guideModel = mongoose.model<Champion & mongoose.Document>('Guide', guideSchema);
 
 export default guideModel;
-
-// guide
-// {
-// 	championId: 11,
-// 	items: [
-// 		{
-// 			index: 1,
-// 			data: [1,2,3,4,5,6]
-// 		}
-// 	],
-// 	skills: [
-// 		{
-// 			index: 1,
-// 			data: [1, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1]
-// 		}
-// 	],
-// 	summoners: [
-// 		{
-// 			index: 1,
-// 			data: [1, 2]
-// 		}
-// 	],
-// 	runes: [
-// 		{
-// 			index: 1,
-// 			data1: [1, 2, 3, 4, 5],
-// 			data2: [1, 2, 3],
-// 			data3: [1, 2, 3],
-// 		}
-// 	],
-// 	position: 1, //1: top, 2: mid, 3: ad, 4: support, 5: jungle
-// 	content: ""
-// }
