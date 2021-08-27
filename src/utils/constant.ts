71fb8d95d680f8ec220ce0b1e733ec0737b2e3db
Tran Thien <thien.tran@saigonvalley.com>
2021-08-27 15:59:10 +0700

    update api get sub2

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -2,4 +2,4 @@ export const championTags = ['Fighter', 'Tank', 'Mage', 'Assassin', 'Support'];
 export const URL_IMAGE_SUMMONER = 'https://api.thuvienmoba.com/spell'
 export const URL_IMAGE_ITEM = 'https://api.thuvienmoba.com/item'
 export const URL_IMAGE_RUNE = 'https://api.thuvienmoba.com/img';
-export const URL_IMAGE_EXTRA_RUNE = 'https://api.thuvienmoba.com/extra-rune';
+export const URL_IMAGE_EXTRA_RUNE = 'https://api.thuvienmoba.com/img/perk-images/StatMods';
